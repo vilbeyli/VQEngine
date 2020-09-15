e27e196de744a066fb4a241e773dc0e4394a7ab7
Volkan <volkanilbeyli@gmail.com>
2020-09-14 19:09:12 -0700

    Add DPI awareness

--- Source/Application/Main.cpp
+++ Source/Application/Main.cpp
@@ -16,9 +16,12 @@
 //
 //	Contact: volkanilbeyli@gmail.com
 
+#include <Windows.h>
+#include <ShellScalingAPI.h>
+#pragma comment(lib, "shcore.lib")
+
 #include <ctime>
 #include <cstdlib>
-#include <Windows.h>
 #include <vector>
 #include <string>
 
@@ -28,7 +31,6 @@
 
 #include "Platform.h"
 #include "VQEngine.h"
-
 
 void ParseCommandLineParameters(FStartupParameters& refStartupParams, PSTR pScmdl)
 {
@@ -170,6 +172,8 @@ void ParseCommandLineParameters(FStartupParameters& refStartupParams, PSTR pScmd
 
 int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, PSTR pScmdl, int iCmdShow)
 {
+	SetProcessDpiAwareness(PROCESS_PER_MONITOR_DPI_AWARE);
+
 	FStartupParameters StartupParameters = {};
 	StartupParameters.hExeInstance = hInst;
 	StartupParameters.iCmdShow = iCmdShow;
